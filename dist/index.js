d344b5061f6f173d32eafadd0c4b3150d2605b6b
JoEun <leejoeun0948@gmail.com>
2020-12-01 09:23:11 -0500

    #02~05 ts theory

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are ${gender}!`;
+};
+console.log(sayHi("Ken", 14, "male"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi =( name:string,age:number,gender:string) :string =>{
+    return `Hello ${name}, you are ${age}, you are ${gender}!`;
+    
+};
+console.log(sayHi("Ken",14,"male"));
+
+export {};
