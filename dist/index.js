b4bec271ad5b9e1f05795e6457c2c76cfedf530a
JoEun <leejoeun0948@gmail.com>
2020-12-01 09:44:28 -0500

    #06 TS interface & class study

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are ${gender}!`;
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const Brian = new Human("Brian", 22, "male");
+const sayHi = (person) => {
+    return `Hello ${person.name},you are ${person.age}, 
+    you are ${person.gender}!`;
 };
-console.log(sayHi("Ken", 14, "male"));
+console.log(sayHi(Brian));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi =( name:string,age:number,gender:string) :string =>{
-    return `Hello ${name}, you are ${age}, you are ${gender}!`;
+class Human{
+   public name:string;
+   public age:number;
+   public gender:string;
+   constructor(name: string, age:number, gender:string){
+    this.name = name;
+    this.age =age;
+    this.gender=gender;
+   }
+}
+const Brian = new Human("Brian",22,"male");
+const sayHi =( person:Human) :string =>{
+    return `Hello ${person.name},you are ${person.age}, 
+    you are ${person.gender}!`;
     
 };
-console.log(sayHi("Ken",14,"male"));
+console.log(sayHi(Brian));
 
 export {};
